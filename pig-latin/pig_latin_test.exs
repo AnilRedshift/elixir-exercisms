291fc49c6f78d4c20b3df4030b6d34715dfb6eba
Anil Kulkarni <akulkarni@avvo.com>
2018-05-23 14:17:29 -0700

    Implement words that start with vowels

--- pig-latin/pig_latin_test.exs
+++ pig-latin/pig_latin_test.exs
@@ -14,22 +14,18 @@ defmodule PigLatinTest do
       assert PigLatin.translate("apple") == "appleay"
     end
 
-    @tag :pending
     test "word beginning with e" do
       assert PigLatin.translate("ear") == "earay"
     end
 
-    @tag :pending
     test "word beginning with i" do
       assert PigLatin.translate("igloo") == "iglooay"
     end
 
-    @tag :pending
     test "word beginning with o" do
       assert PigLatin.translate("object") == "objectay"
     end
 
-    @tag :pending
     test "word beginning with u" do
       assert PigLatin.translate("under") == "underay"
     end
