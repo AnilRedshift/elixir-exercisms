da231eba0f31503145e516ce2744430cf2a9d729
Anil Kulkarni <akulkarni@avvo.com>
2018-05-23 12:21:46 -0700

    implement of_codon

--- protein-translation/protein_translation.exs
+++ protein-translation/protein_translation.exs
@@ -27,7 +27,25 @@ defmodule ProteinTranslation do
   UAG -> STOP
   UGA -> STOP
   """
+  @codons %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP",
+  }
   @spec of_codon(String.t()) :: {atom, String.t()}
-  def of_codon(codon) do
-  end
+  def of_codon(codon), do: Map.fetch(@codons, codon)
 end
