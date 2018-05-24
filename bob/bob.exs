594512161d047b50c9cb27b87aa6bddb8b4d71f5
Anil Kulkarni <akulkarni@avvo.com>
2018-05-24 14:57:02 -0700

    handle question

--- bob/bob.exs
+++ bob/bob.exs
@@ -3,11 +3,13 @@ defmodule Bob do
   @responses %{
     default: "Whatever.",
     shouting: "Whoa, chill out!",
+    question: "Sure.",
   }
 
   def hey(input) do
     cond do
       String.match?(input, ~r/^[^a-z]+$/) -> @responses.shouting
+      String.ends_with?(input, "?") -> @responses.question
       true -> @responses.default
     end
   end
