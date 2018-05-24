11f3f91799fef22476d55a8906631d2441d7dccf
Anil Kulkarni <akulkarni@avvo.com>
2018-05-24 14:47:34 -0700

    Solve age on earth

--- space-age/space_age.exs
+++ space-age/space_age.exs
@@ -13,7 +13,22 @@ defmodule SpaceAge do
   Return the number of years a person that has lived for 'seconds' seconds is
   aged on 'planet'.
   """
+  @earth_orbit_seconds 31557600
+  @orbit_ratios %{
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1.0,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
+
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
+    earth_years(seconds)
   end
+
+  defp earth_years(seconds), do: seconds / @earth_orbit_seconds
 end
