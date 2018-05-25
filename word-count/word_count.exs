2960d8988be62ead8ae3c27a59c4792d96a55e49
Anil Kulkarni <akulkarni@avvo.com>
2018-05-24 17:48:32 -0700

    fix punctuation for unicode

--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -9,7 +9,7 @@ defmodule Words do
     sentence
     |> String.downcase
     |> String.replace("_", " ")
-    |> String.replace(~r/[^a-z0-9 -]/, "")
+    |> String.replace(~r/\p{S}|\p{Ps}\p{Pe}|\p{Pi}|\p{Pf}|\p{Po}/u, "")
     |> String.split()
     |> Enum.reduce(%{}, fn (word, acc) ->
       Map.update(acc, word, 1, &(&1 + 1))
