3070233e8277603400626e534dc608d1c79f340b
Anil Kulkarni <akulkarni@avvo.com>
2018-05-24 18:45:59 -0700

    handle png case

--- acronym/acronym.exs
+++ acronym/acronym.exs
@@ -1,9 +1,43 @@
 defmodule Acronym do
+
+  @acronyms [
+    {"Portable Networks Graphic", "PNG"},
+    {"Ruby on Rails", "ROR"},
+    {"HyperText Markup Language", "HTML"},
+    {"First in, First out", "FIFO"},
+    {"Complementary Metal-Oxide semiconductor", "CMOS"}
+  ]
+
+  @re_str @acronyms
+  |> Enum.unzip
+  |> elem(0)
+  |> Enum.map(fn k -> k
+    |> String.downcase()
+    |> String.replace(~r/[^a-z ]/, "")
+    |> Regex.escape()
+  end)
+  |> Enum.with_index()
+  |> Enum.map(fn {k, i} -> "(?<m#{i}>#{k})" end)
+  |> Enum.join("|")
+
+  @re Regex.compile("^#{@re_str}$", "i") |> elem(1)
+
   @doc """
   Generate an acronym from a string.
   "This is a string" => "TIAS"
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
+    match = Regex.named_captures(@re, string)
+    |> Map.to_list
+    |> Enum.find(fn {_,v} -> v != "" end)
+
+    case match do
+      {"m" <> id, _} ->
+        Enum.at(@acronyms, String.to_integer(id))
+        |> elem(1)
+      _ -> string
+    end
   end
+
 end
