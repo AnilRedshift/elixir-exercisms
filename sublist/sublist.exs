194a5acf08f9017517c208899d4243ea34eed2e7
Anil Kulkarni <akulkarni@avvo.com>
2018-07-24 13:06:35 -0700

    sublist skeleton

--- sublist/sublist.exs
+++ sublist/sublist.exs
@@ -4,4 +4,14 @@ defmodule Sublist do
   and if not whether it is equal or unequal to the second list.
   """
   def compare(a, b) when a == b, do: :equal
+  def compare(a, b) do
+    case sublist?(a, b) do
+      true -> :sublist
+      false -> :superlist
+    end
+  end
+
+  defp sublist?(a, b) do
+    true
+  end
 end
