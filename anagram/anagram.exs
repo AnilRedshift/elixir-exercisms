355d7986ca76f8a71889d366feacb64739de9ba4
Anil Kulkarni <akulkarni@avvo.com>
2018-07-24 16:36:31 -0700

    implement anagrams

--- anagram/anagram.exs
+++ anagram/anagram.exs
@@ -4,5 +4,14 @@ defmodule Anagram do
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
   def match(base, candidates) do
+    Enum.filter(candidates, &(hash(&1) == hash(base)))
+    |> Enum.filter(&(String.downcase(&1) != String.downcase(base)))
+  end
+
+  defp hash(word) do
+    updater = &(&1 + 1)
+    String.downcase(word)
+    |> String.graphemes()
+    |> Enum.reduce(%{}, &Map.update(&2, &1, 1, updater))
   end
 end
