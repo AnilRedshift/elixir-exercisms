f55e1aa153a02bab3905ada752fb26501230fd41
Anil Kulkarni <akulkarni@avvo.com>
2018-07-26 16:32:13 -0700

    convert nums

--- all-your-base/all-your-base.exs
+++ all-your-base/all-your-base.exs
@@ -6,5 +6,38 @@ defmodule AllYourBase do
 
   @spec convert(list, integer, integer) :: list
   def convert(digits, base_a, base_b) do
+    case valid?(digits, base_a, base_b) do
+      false -> nil
+      true -> parse(digits, base_a) |> to_base(base_b)
+    end
+  end
+
+  defp parse(digits, base_a) do
+    Enum.reverse(digits)
+      |> Enum.with_index()
+      |> Enum.map(fn {num, power} ->
+        :math.pow(base_a, power) * num
+      end)
+      |> Enum.sum()
+      |> trunc()
+  end
+
+  defp to_base(0, _base), do: [0]
+  defp to_base(num, base) do
+    num_digits = :math.log(num) / :math.log(base)
+    |> trunc()
+
+    (for x <- 0..num_digits, do: :math.pow(base, x) |> trunc())
+    |> Enum.reverse()
+    |> Enum.map_reduce(num, fn place, num ->
+      {div(num, place), rem(num, place)}
+    end)
+    |> elem(0)
+  end
+
+  defp valid?([], _, _), do: false
+  defp valid?(_, base_a, base_b) when base_a <= 1 or base_b <= 1, do: false
+  defp valid?(digits, base_a, _base_b) do
+    Enum.all?(digits, &(&1 >= 0 && &1 < base_a))
   end
 end
