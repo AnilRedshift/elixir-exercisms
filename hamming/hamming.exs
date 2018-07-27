6ec7a0e3fa2460aca05e06acfecbde74dbf2782c
Anil Kulkarni <akulkarni@avvo.com>
2018-07-27 10:05:38 -0700

    implement hamming

--- hamming/hamming.exs
+++ hamming/hamming.exs
@@ -8,6 +8,11 @@ defmodule Hamming do
   {:ok, 4}
   """
   @spec hamming_distance([char], [char]) :: non_neg_integer
+  def hamming_distance(strand1, strand2) when length(strand1) != length(strand2), do: {:error, "Lists must be the same length"}
   def hamming_distance(strand1, strand2) do
+    count = Enum.zip(strand1, strand2)
+    |> Enum.reject(fn {a,b} -> a == b end)
+    |> Enum.count()
+    {:ok, count}
   end
 end
