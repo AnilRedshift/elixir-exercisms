eb1a6532b50042031c2f14433e40f3fc057186ea
Anil Kulkarni <akulkarni@avvo.com>
2018-07-30 17:59:25 -0700

    everything a reduce

--- list-ops/list_ops.exs
+++ list-ops/list_ops.exs
@@ -7,27 +7,21 @@ defmodule ListOps do
   # automatically imported) and so shouldn't be used either.
 
   @spec count(list) :: non_neg_integer
-  def count(l), do: count(l, 0)
-  def count([], sum), do: sum
-  def count([_item | rest], sum), do: count(rest, sum + 1)
+  def count(l), do: reduce(l, 0, fn _item, sum -> sum + 1 end)
 
   @spec reverse(list) :: list
-  def reverse(l), do: reduce(l, [], fn (item, reversed) -> [item | reversed] end)
+  def reverse(l), do: reduce(l, [], &[&1 | &2])
 
   @spec map(list, (any -> any)) :: list
-  def map([], _f), do: []
-  def map([first | rest], f), do: [f.(first) | map(rest, f)]
+  def map(l, f), do: reduce(l, [], &[f.(&1) | &2]) |> reverse()
 
-  @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f), do: filter(l, f, []) |> reverse()
-  def filter([], _f, res), do: res
-  def filter([first | rest], f, res) do
-    res = case !!f.(first) do
-      true -> [first | res]
-      false -> res
+  def filter(l, f), do: reduce(l, [], fn item, acc ->
+    case !!f.(item) do
+      true -> [item | acc]
+      false -> acc
     end
-    filter(rest, f, res)
-  end
+  end)
+  |> reverse()
 
   @type acc :: any
   @spec reduce(list, acc, (any, acc -> acc)) :: acc
@@ -37,10 +31,7 @@ defmodule ListOps do
     reduce(rest, acc, f)
   end
 
-  def append(a, b) do
-    reverse(a)
-    |> reduce(b, &[&1 | &2])
-  end
+  def append(a, b), do: reverse(a) |> reduce(b, &[&1 | &2])
 
   @spec concat([[any]]) :: [any]
   def concat(ll) do
