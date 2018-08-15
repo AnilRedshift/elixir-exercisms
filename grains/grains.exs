4998b84fee51caf2ac6aa674f14d3da2c3f434e9
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-08-14 22:39:07 -0700

    switch to using bitshift

--- grains/grains.exs
+++ grains/grains.exs
@@ -1,4 +1,5 @@
 defmodule Grains do
+  use Bitwise
   @doc """
   Calculate two to the power of the input minus one.
   """
@@ -6,15 +7,8 @@ defmodule Grains do
   def square(num) when num < 1 or num > 64 do
     {:error, "The requested square must be between 1 and 64 (inclusive)"}
   end
-  def square(num) do
-    squared = Stream.unfold(0, fn
-      0 -> {1, 1}
-      n -> {n * 2, n * 2}
-    end)
-    |> Enum.take(num)
-    |> List.last()
-    {:ok, squared}
-  end
+
+  def square(num), do: {:ok, bsl(1, num - 1)}
 
   @doc """
   Adds square of each number from 1 to 64.
