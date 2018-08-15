3b002469047b89fc6a525b1a4defd29065b1f394
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-08-14 22:36:13 -0700

    simple solution

--- grains/grains.exs
+++ grains/grains.exs
@@ -3,7 +3,17 @@ defmodule Grains do
   Calculate two to the power of the input minus one.
   """
   @spec square(pos_integer) :: pos_integer
-  def square(number) do
+  def square(num) when num < 1 or num > 64 do
+    {:error, "The requested square must be between 1 and 64 (inclusive)"}
+  end
+  def square(num) do
+    squared = Stream.unfold(0, fn
+      0 -> {1, 1}
+      n -> {n * 2, n * 2}
+    end)
+    |> Enum.take(num)
+    |> List.last()
+    {:ok, squared}
   end
 
   @doc """
@@ -11,5 +21,8 @@ defmodule Grains do
   """
   @spec total :: pos_integer
   def total do
+    sum = (for n <- 1..64, do: square(n) |> elem(1))
+    |> Enum.sum()
+    {:ok, sum}
   end
 end
