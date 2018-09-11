0248b5a06adbde185d273b60b1ffc163d8f0cba5
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-09-10 18:21:55 -0700

    implement change by brute force

--- change/change.exs
+++ change/change.exs
@@ -16,6 +16,36 @@ defmodule Change do
   """
 
   @spec generate(list, integer) :: {:ok, list} | {:error, String.t()}
+
   def generate(coins, target) do
+    do_generate(coins, target, %{0 => []})
+    |> Map.fetch(target)
+    |> case do
+      {:ok, val} -> {:ok, val}
+      :error -> {:error, "cannot change"}
+    end
+  end
+
+  defp do_generate(coins, target, cache) when target <= 0, do: cache
+  defp do_generate(coins, target, cache) do
+    case Map.fetch(cache, target) do
+      {:ok, change} -> cache
+      :error ->
+        Enum.reduce(coins, cache, fn coin, cache ->
+        cache = do_generate(coins, target - coin, cache)
+        case cache[target - coin] do
+          nil -> cache
+          _change -> update_cache(cache, target, coin)
+        end
+      end)
+    end
+  end
+
+  defp update_cache(cache, target, coin) do
+    change = [coin | cache[target - coin]]
+    Map.update(cache, target, change, fn
+      existing when length(change) < length(existing) -> change
+      existing -> existing
+    end)
   end
 end
